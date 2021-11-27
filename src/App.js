5310985fe6964437adba65c68fcb70d380d7dbdd
MD <dayclawtel@gmail.com>
2021-11-26 23:08:20 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Search from "./Search";
+import Coin from "./Coin";
+import NavBar from './NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+    .then(response => response.json())
+    .then(data => setCoins(data))
+  }, [])
+
+  const allCoins = coins.map(({ id, name, symbol, current_price, image, price_change_percentage_24h, market_cap }) => {
+    return (
+    <div key={id}>
+        <Coin    
+            name={name}
+            symbol={symbol}
+            price={current_price}
+            image={image}
+            priceChange={price_change_percentage_24h.toFixed(2)}
+            marketCap={market_cap}
+        />
     </div>
-  );
+  )})
+
+    return (
+      
+      <div className="App">
+      <NavBar />
+      <Search coins={coins} />
+      <h1 className="coin-text">CryptoCo</h1>
+            {allCoins}
+      </div>
+      
+    )
 }
 
-export default App;
+export default App
---
+++ src/Coin.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Favorites from './Favorites';
+
+function Coin({ id, coin, name, price, image, priceChange, marketCap, favorites }) {
+
+    return (
+        <div key={id} className='coin-container'>
+            <div className='coin-row'>
+                <div className="coin">
+                    <img src={image} alt='crypto' />
+                    <p className="coin-symbol">{coin}</p>
+                    <p className="coin-name">{name}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">${price}</p>
+                    <p className={`coin_percent ${priceChange < 0 ? "red" : "green"}`}>{priceChange}%</p>
+                    <p className="coin-marketcap">Market Cap: ${marketCap.toLocaleString()}</p>
+                    <Favorites id={id} coin={coin} price={price} image={image} favorites={favorites} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coin;
